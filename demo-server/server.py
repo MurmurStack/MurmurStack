c5e08b18e90fd185505e21a565b84a310202cdb4
Aden  Al-Hardan <adenalhardan@gmail.com>
2025-05-04 11:53:30 -0700

    Implement control web socket on demo server

--- demo-server/server.py
+++ demo-server/server.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request, Response, WebSocket, status
-from enum import Enum
+from fastapi import FastAPI, Request, Response, WebSocket, status, WebSocketDisconnect
+from enum import StrEnum
 from transcribe import get_transcript
-from dataclasses import dataclass
+from constants import SAMPLE_RATE_HZ
+from dataclasses import dataclass, asdict
 from collections import defaultdict
 
-class Status(Enum):
+class Status(StrEnum):
   Success = "success"
   NoSpeech = "no_voice_detected"
   Error = "error"
@@ -20,8 +21,7 @@ buffs = defaultdict(bytes)
 MAX_BUFFER_SIZE_S = 5.0
 
 def get_buffer_size_s(buff: bytes) -> float:
-  sample_rate = 16000
-  return len(buff) / sample_rate # this may not be right
+  return len(buff) / SAMPLE_RATE_HZ # this may not be right
 
 @app.websocket("/ws-murmur")
 async def upgrade_murmur(ws: WebSocket):
@@ -33,30 +33,33 @@ async def upgrade_murmur(ws: WebSocket):
   await ws.close(code=status.HTTP_501_NOT_IMPLEMENTED)
 
 
-@app.websocket("/ws-control")
-async def upgrade_control(ws: WebSocket):
-  client_id = ws.headers.get("client-id")
-
-  if client_id is None:
-    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
-
+@app.websocket("/ws-control/{client_id}")
+async def upgrade_control(ws: WebSocket, client_id: str):
   await ws.accept()
 
-  while True:
-    data = await ws.receive_bytes()
-    buffs[client_id] += data
+  try:
+    while True:
+      data = await ws.receive_bytes()
 
-    if get_buffer_size_s(buffs[client_id]) >= MAX_BUFFER_SIZE_S:
-      try:
-        transcript = get_transcript(data)
-        buffs[client_id] = b''
-      except:
-        await ws.send_json(TranscriptMessage(status=Status.Error))
+      if len(data) < 400:
+        continue
 
-      if transcript:
-        await ws.send_json(TranscriptMessage(status=Status.Success, transcript=transcript))
-      else:
-        await ws.send_json(TranscriptMessage(status=Status.NoSpeech))
+      buffs[client_id] += data
+
+      if get_buffer_size_s(buffs[client_id]) >= MAX_BUFFER_SIZE_S:
+        try:
+          transcript = get_transcript(buffs[client_id])
+          buffs[client_id] = b''
+        except:
+          await ws.send_json(asdict(TranscriptMessage(status=Status.Error)))
+
+        if transcript:
+          await ws.send_json(asdict(TranscriptMessage(status=Status.Success, transcript=transcript)))
+        else:
+          await ws.send_json(asdict(TranscriptMessage(status=Status.NoSpeech)))
+
+  except WebSocketDisconnect:
+    print(f"{client_id} disconnected")
 
 
 @app.get("/get-metrics")
--- demo-server/transcribe.py
+++ demo-server/transcribe.py
@@ -1,12 +1,32 @@
 import openai
+import numpy as np
 import os
+import tempfile
+from constants import SAMPLE_RATE_HZ
+import wave
 
-def get_transcript(data: bytes) -> str:
-  '''
-  openai.audio.transcriptions.create(
-    model="whisper-1",
-    file=audio_file,
-    response_format="text"
-  )
-  '''
-  return "NoImpl"
+def get_transcript(buff: bytes) -> str:
+  openai.api_key = os.environ.get('API_KEY_OPENAI')
+
+  buff = (
+    np.frombuffer(buff, dtype=np.float32) * np.iinfo(np.int16).max
+  ).astype(np.int16).tobytes()
+
+  if buff.size < 100:
+    return ""
+
+  with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as file:
+    with wave.open(file.name, "wb") as wav:
+      wav.setnchannels(1)
+      wav.setsampwidth(2)
+      wav.setframerate(SAMPLE_RATE_HZ)
+      wav.writeframes(buff)
+
+    with open(file.name, "rb") as wav:
+      transcript = openai.audio.transcriptions.create(
+          model="whisper-1",
+          file=wav,
+          response_format="text"
+      )
+
+  return transcript
